7131dd953db987d55f2165956d72427da7c018da
Narit Charoenval <naritsociety@gmail.com>
2020-09-03 16:42:22 +0700

    Add ModelName class

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
-import datetime
+from enum import Enum
+# import datetime
 
 app = FastAPI()
 
@@ -20,6 +21,12 @@ print(post_id)
 """
 
 
+class ModelName(str, Enum):
+    alexnet = "alexnet"
+    resnet = "resnet"
+    lenet = "lenet"
+
+
 @app.get("/")
 async def root():
     return {
@@ -28,6 +35,17 @@ async def root():
     }
 
 
-@app.get("/data/{t}")
+@app.get("/sensor/{t}")
 async def get_sensor_data(t: float):
     return {"Temperature:": t}
+
+
+@app.get("/model/{model_name}")
+async def get_model(model_name: ModelName):
+    if model_name == ModelName.alexnet:
+        return {"model_name": model_name, "message": "Deep Learning FTW!"}
+
+    if model_name.value == "lenet":
+        return {"model_name:": model_name, "message": "LeCNN all the images"}
+
+    return {"model_name:": model_name, "message": "Have some residuals"}
