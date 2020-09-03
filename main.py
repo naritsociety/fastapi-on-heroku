9c4e3174a673cfa92e0c6f4bde05d6421d4a7ed4
Narit Charoenval <naritsociety@gmail.com>
2020-09-03 17:54:59 +0700

    Multiple path and query parameters

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from fastapi import FastAPI
 from pymongo import MongoClient
 from enum import Enum
+from typing import Optional
 # import datetime
 
 app = FastAPI()
 
 client = MongoClient("mongodb+srv://narit:Crazydog2029@cluster0.58ggi.mongodb.net/fastapi?retryWrites=true&w=majority")
 db = client.fastapi
+
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
 """
 post = {"author": "Mikey",
@@ -49,3 +52,21 @@ async def get_model(model_name: ModelName):
         return {"model_name:": model_name, "message": "LeCNN all the images"}
 
     return {"model_name:": model_name, "message": "Have some residuals"}
+
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def read_user_item(
+        user_id: int,
+        item_id: str,
+        q: Optional[str] = None,
+        short: bool = False
+):
+
+    item = {"item_id": item_id, "owner_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
