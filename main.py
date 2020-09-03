c19c3a780d121a9a2869bc2a1c115a18a0ccf42e
Narit Charoenval <naritsociety@gmail.com>
2020-09-03 15:13:13 +0700

    Add mongodb connect and insert test

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from pymongo import MongoClient
+import datetime
 
 app = FastAPI()
 
+client = MongoClient("mongodb+srv://narit:Crazydog2029@cluster0.58ggi.mongodb.net/fastapi?retryWrites=true&w=majority")
+db = client.fastapi
+
+post = {"author": "Mikey",
+         "text": "My first blog post!",
+         "tags": ["mongodb", "python", "pymongo"],
+         "date": datetime.datetime.utcnow()}
+
+posts = db.posts
+post_id = posts.insert_one(post).inserted_id
+print(post_id)
 
 @app.get("/")
 def hello():
     return {
         "message": "Hello Heroku",
         "framework": "FastAPI",
+        "post_id": post_id
     }
